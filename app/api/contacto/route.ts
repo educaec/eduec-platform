821adefe1db9392bb1a401431e8ca219b99b2ea8
Pablo Andres Agila Hidalgo <pablo@MacBook-Air-de-Pablo.local>
2025-11-21 01:20:35 -0500

    Fix resend API key

--- app/api/contacto/route.ts
+++ app/api/contacto/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function POST(req: Request) {
   try {
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
 
     const data = await resend.emails.send({
       from: "EduEc <onboarding@resend.dev>",
-      to: "TU-CORREO-AQUÍ", // <-- reemplázalo por el correo donde quieres recibir mensajes
+      to: "TU-CORREO-REAL",  
       subject: `Nuevo mensaje de contacto (${motivo})`,
       html: `
         <h2>Nuevo mensaje desde la plataforma EduEc</h2>
